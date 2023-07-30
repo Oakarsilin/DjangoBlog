3669876fff47ff5a868f2e4e4ef4aa839e9d7a61
Oakarsilin <oakarsilin@gmail.com>
2023-07-30 12:22:26 +0630

    update core setting urls

--- core_setting/urls.py
+++ core_setting/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Blogging.urls'), name='Blogging')
+    path('', include('Blogging.urls'), name='Blogging'),
+    path('register/', include('user.urls'), name='user')
 ]
 
 urlpatterns += static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
