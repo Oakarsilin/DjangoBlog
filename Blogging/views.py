9a38d3724289d168be3a1e138338b6f55ee502dd
Oakarsilin <oakarsilin@gmail.com>
2023-07-30 12:16:02 +0630

    Update

--- Blogging/views.py
+++ Blogging/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from .forms import Registration
-from .models import User
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home_view (request):
@@ -16,19 +15,3 @@ def home_view (request):
 
 def about_view (request):
     return render (request, 'about.html')
-
-def register_view (request):
-    form = Registration(request.POST)
-    # if request.method == 'POST':
-    #     username = request.POST.get ('username')
-    #     password = request.POST['password']
-    #     user.username = username
-    #     user.password = password
-    #     user.save()
-    #     print (username, password)
-    #     return render (request, 'register.html', {'form' : form})
-    if form.is_valid():
-        cleaned_form = form.clean()
-        User.objects.create (**cleaned_form)
-        return render (request, 'home.html')
-    return render (request, 'register.html',  {'form' : form})
